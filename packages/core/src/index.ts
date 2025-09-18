b78f6015b8e0f2d464f29b5cf986b8b31d2eba20
minkyu_Park <kyle970320@farmzonesnc.com>
2025-09-18 10:09:25 +0900

    feat: replace rollup-copy to import

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./components";
 export * from "./types";
+import "./tailwind/plugin.css";
--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import copy from "rollup-plugin-copy";
 
 export default defineConfig({
   build: {
@@ -22,20 +21,6 @@ export default defineConfig({
       compilerOptions: {
         baseUrl: "",
       },
-    }),
-    copy({
-      targets: [
-        // tailwind의 plugin을 복사해서 생성
-        {
-          src: "./src/tailwind/*.css",
-          dest: "dist",
-        },
-        {
-          src: "./css.d.ts",
-          dest: "dist",
-        },
-      ],
-      hook: "writeBundle", // 번들링 후 실행
     }),
   ],
 });
